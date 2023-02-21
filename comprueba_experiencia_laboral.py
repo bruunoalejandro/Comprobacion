24176d7b9e913f1b43fc11b12b8a8cab73162fa4
Bruno Fernandes <125995286+bruunoalejandro@users.noreply.github.com>
2023-02-21 14:48:20 -0300

    Add files via upload

---
+++ comprueba_experiencia_laboral.py
@@ -0,0 +1,10 @@
+cat = int(input("Ingrese cual categoria tienes: "))
+exp = int(input("Ingrese cuantos años de experiencia tienes: "))
+if cat == 3 and exp >= 5:
+    print("Usted esta calificado para ser nuestro empleado!")
+elif cat == 4 and exp >= 5:
+    print("Usted esta calificado para ser nuestro empleado!")
+elif cat == 2 and exp >= 7:
+    print("Usted esta calificado para ser nuestro empleado!")
+else:
+    print("Usted no esta calificado para ser nuestro empleado.")
